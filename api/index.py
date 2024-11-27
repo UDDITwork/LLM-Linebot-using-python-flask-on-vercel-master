1edc7d5e9ac9d58dd840deb5430b342187d85ba4
UDDITwork <168342115+UDDITwork@users.noreply.github.com>
2024-11-28 02:38:09 +0530

    Update index.py

--- api/index.py
+++ api/index.py
@@ -3,64 +3,69 @@ from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
 from linebot.models import MessageEvent, TextMessage, TextSendMessage
 from api.chatgpt import ChatGPT
-
 import os
 
+# Initialize Line Bot API with access token
 line_bot_api = LineBotApi(os.getenv("LINE_CHANNEL_ACCESS_TOKEN"))
 line_handler = WebhookHandler(os.getenv("LINE_CHANNEL_SECRET"))
-working_status = os.getenv("DEFALUT_TALKING", default = "true").lower() == "true"
+# Set default talking status from environment variable
+working_status = os.getenv("DEFALUT_TALKING", default="true").lower() == "true"
 
 app = Flask(__name__)
 chatgpt = ChatGPT()
 
-# domain root
+# Root route
 @app.route('/')
 def home():
     return 'Hello, World!'
 
+# Webhook endpoint for Line Bot
 @app.route("/webhook", methods=['POST'])
 def callback():
-    # get X-Line-Signature header value
     signature = request.headers['X-Line-Signature']
-    # get request body as text
     body = request.get_data(as_text=True)
     app.logger.info("Request body: " + body)
-    # handle webhook body
+    
     try:
         line_handler.handle(body, signature)
     except InvalidSignatureError:
         abort(400)
     return 'OK'
 
-
 @line_handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     global working_status
+    
     if event.message.type != "text":
         return
 
-    if event.message.text == "說話":
+    # Command to enable talking
+    if event.message.text == "speak":  
         working_status = True
         line_bot_api.reply_message(
             event.reply_token,
-            TextSendMessage(text="我可以說話囉，歡迎來跟我互動 ^_^ "))
+            TextSendMessage(text="I can talk now, feel free to interact with me ^_^")  
+        )
         return
 
-    if event.message.text == "閉嘴":
+    # Command to disable talking
+    if event.message.text == "quiet": 
         working_status = False
         line_bot_api.reply_message(
             event.reply_token,
-            TextSendMessage(text="好的，我乖乖閉嘴 > <，如果想要我繼續說話，請跟我說 「說話」 > <"))
+            TextSendMessage(text="Okay, I'll be quiet now. If you want me to talk again, please say 'speak' > <") 
+        )
         return
 
+    # Handle normal conversation when working_status is True
     if working_status:
         chatgpt.add_msg(f"HUMAN:{event.message.text}?\n")
         reply_msg = chatgpt.get_response().replace("AI:", "", 1)
         chatgpt.add_msg(f"AI:{reply_msg}\n")
         line_bot_api.reply_message(
             event.reply_token,
-            TextSendMessage(text=reply_msg))
-
+            TextSendMessage(text=reply_msg)
+        )
 
 if __name__ == "__main__":
     app.run()
